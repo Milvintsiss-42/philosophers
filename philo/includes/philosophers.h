7c679a4058aae4b9edc118e9cb12fe071f9b4f21
Milvintsiss <milvintsiss@gmail.com>
2022-08-26 13:51:11 +0200

    Adds files for mandatory part

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/29 15:45:28 by ple-stra          #+#    #+#             */
+/*   Updated: 2022/08/26 13:43:38 by ple-stra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <sys/errno.h>
+
+# define ERR_WRG_NB_ARG			"Incorrect number of arguments"
+# define ERR_UNKNOWN			"Unknown error"
+
+#endif
