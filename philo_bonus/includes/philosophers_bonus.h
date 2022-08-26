451930e56ed15e57d45b3d7117e37a5a8c65a11f
Milvintsiss <milvintsiss@gmail.com>
2022-08-26 13:51:31 +0200

    Adds files for bonus part

---
+++ philo_bonus/includes/philosophers_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers_bonus.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/26 13:47:58 by ple-stra          #+#    #+#             */
+/*   Updated: 2022/08/26 13:48:12 by ple-stra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_BONUS_H
+# define PHILOSOPHERS_BONUS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <sys/errno.h>
+
+# define ERR_WRG_NB_ARG			"Incorrect number of arguments"
+# define ERR_UNKNOWN			"Unknown error"
+
+#endif
