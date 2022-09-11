75c2eabb823d49ec1b15612eeb2217d906c2b4fb
Milvintsiss <milvintsiss@gmail.com>
2022-09-11 23:04:06 +0200

    Stops using exit method

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: milvintsiss <milvintsiss@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 13:44:19 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/09/06 21:36:23 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/11 23:03:33 by milvintsiss      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-static void	init_exec_data(t_exec_data *exec_data, int argc, char const **argv)
+static int	init_exec_data(t_exec_data *exec_data, int argc, char const **argv)
 {
 	if (argc < 5 || argc > 6)
-		exit(ft_perror(ERR_WRG_NB_ARG, -1));
+		return (ft_perror(ERR_WRG_NB_ARG, -1));
 	exec_data->nb_philo = get_input(argv[1]);
 	exec_data->t_to_die = get_input(argv[2]);
 	exec_data->t_to_eat = get_input(argv[3]);
@@ -27,15 +27,17 @@ static void	init_exec_data(t_exec_data *exec_data, int argc, char const **argv)
 	if (exec_data->nb_philo <= 0 || exec_data->t_to_die == -1
 		|| exec_data->t_to_eat == -1 || exec_data->t_to_sleep == -1
 		|| exec_data->nb_of_dinners == -1)
-		exit(ft_perror(ERR_WRG_ARG, -1));
+		return (ft_perror(ERR_WRG_ARG, -1));
 	exec_data->init_timestamp = 0;
 	exec_data->init_timestamp = gettimestamp(*exec_data);
+	return (0);
 }
 
 int	main(int argc, char const **argv)
 {
 	t_exec_data	exec_data;
 
-	init_exec_data(&exec_data, argc, argv);
+	if (init_exec_data(&exec_data, argc, argv) == -1)
+		return (-1);
 	return (0);
 }
