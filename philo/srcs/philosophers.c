f4e677147b9b7deb659845816c2f863dfdf4ad0e
Milvintsiss <milvintsiss@gmail.com>
2022-09-20 15:33:55 +0200

    Converts milliseconds to microseconds

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 13:44:19 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/09/19 22:26:45 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/20 15:33:45 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ static int	init_exec_data(t_exec_data *exec_data, int argc, char const **argv)
 	if (argc < 5 || argc > 6)
 		return (ft_perror(exec_data, ERR_WRG_NB_ARG, 2));
 	exec_data->nb_philo = get_input(argv[1]);
-	exec_data->t_to_die = get_input(argv[2]);
-	exec_data->t_to_eat = get_input(argv[3]);
-	exec_data->t_to_sleep = get_input(argv[4]);
+	exec_data->t_to_die = get_input(argv[2]) * 1000;
+	exec_data->t_to_eat = get_input(argv[3]) * 1000;
+	exec_data->t_to_sleep = get_input(argv[4]) * 1000;
 	if (argc == 6)
 		exec_data->nb_of_dinners = get_input(argv[5]);
 	else
 		exec_data->nb_of_dinners = INTMAX;
-	if (exec_data->nb_philo <= 0 || exec_data->t_to_die == -1
-		|| exec_data->t_to_eat == -1 || exec_data->t_to_sleep == -1
+	if (exec_data->nb_philo <= 0 || exec_data->t_to_die < 0
+		|| exec_data->t_to_eat < 0 || exec_data->t_to_sleep < 0
 		|| exec_data->nb_of_dinners == -1)
 		return (ft_perror(exec_data, ERR_WRG_ARG, 3));
 	exec_data->init_timestamp = 0;
