ed900532bf5f5185a7ef92b480106938092768b7
Milvintsiss <milvintsiss@gmail.com>
2022-09-21 16:23:08 +0200

    Verifies if philosopher don't die while eating

--- philo/srcs/eat.c
+++ philo/srcs/eat.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 15:25:06 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/09/20 15:39:33 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/21 16:22:42 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,32 @@ static void	take_fork_action(t_philo philo, int is_right)
 		log_action(*philo.exec_data, philo.id, ACT_TAKE_RIGHT_FORK);
 	else
 		log_action(*philo.exec_data, philo.id, ACT_TAKE_LEFT_FORK);
+}
+
+static int	take_fork(t_philo *philo, int fork_id, int is_right)
+{
+	if (is_dead_or_stop(philo))
+		return (died_or_stop(philo));
+	if (pthread_mutex_lock(&philo->exec_data->forks[fork_id]) != 0)
+		return (ft_perror(philo->exec_data, ERR_UNKNOWN, 7));
+	take_fork_action(*philo, is_right);
+	return (1);
+}
+
+static int	take_dinner_and_release_forks(t_philo *philo,
+	int first_fork, int second_fork)
+{
+	if (is_dead_or_stop(philo))
+		return (died_or_stop(philo));
+	philo->t_last_meal = gettimestamp(*philo->exec_data);
+	log_action(*philo->exec_data, philo->id, ACT_IS_EATING);
+	usleep(philo->exec_data->t_to_eat);
+	if (is_dead_or_stop(philo))
+		return (died_or_stop(philo));
+	pthread_mutex_unlock(&philo->exec_data->forks[first_fork]);
+	pthread_mutex_unlock(&philo->exec_data->forks[second_fork]);
+	philo->nb_of_dinners_eat++;
+	return (1);
 }
 
 int	eat(t_philo *philo)
@@ -34,16 +60,11 @@ int	eat(t_philo *philo)
 		+ (philo->id % 2 == 0) * left_fork;
 	second_fork = (philo->id % 2 == 0) * right_fork
 		+ (philo->id % 2 == 1) * left_fork;
-	if (pthread_mutex_lock(&philo->exec_data->forks[first_fork]) != 0)
-		return (ft_perror(philo->exec_data, ERR_UNKNOWN, 7));
-	take_fork_action(*philo, first_fork == right_fork);
-	if (pthread_mutex_lock(&philo->exec_data->forks[second_fork]) != 0)
-		return (ft_perror(philo->exec_data, ERR_UNKNOWN, 7));
-	take_fork_action(*philo, second_fork == right_fork);
-	log_action(*philo->exec_data, philo->id, ACT_IS_EATING);
-	usleep(philo->exec_data->t_to_eat);
-	pthread_mutex_unlock(&philo->exec_data->forks[first_fork]);
-	pthread_mutex_unlock(&philo->exec_data->forks[second_fork]);
-	philo->nb_of_dinners_eat++;
+	if (!take_fork(philo, first_fork, first_fork == right_fork))
+		return (0);
+	if (!take_fork(philo, second_fork, second_fork == right_fork))
+		return (0);
+	if (!take_dinner_and_release_forks(philo, first_fork, second_fork))
+		return (0);
 	return (1);
 }
