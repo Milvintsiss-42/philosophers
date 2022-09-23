d1f280129d70a237ecfeeb089cb859e2a4647039
Milvintsiss <milvintsiss@gmail.com>
2022-09-23 21:41:25 +0200

    Fixes logging think action after think time

--- philo/srcs/think.c
+++ philo/srcs/think.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 15:27:35 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/09/22 14:14:23 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/09/23 21:35:45 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	think(t_philo *philo)
 {
+	log_action(*philo->exec_data, philo->id, ACT_IS_THINKING);
 	usleep(philo->exec_data->t_to_think * 1000);
 	if (is_dead_or_stop(philo))
 		return (died_or_stop(philo));
-	log_action(*philo->exec_data, philo->id, ACT_IS_THINKING);
 	return (1);
 }
